c06fef6b3c8bff423de977f646bb31c7b443a2a9
peterhwang2010 <inhohwang2010@gmail.com>
2015-10-03 09:11:15 -0400

    added Register page and the routes for register

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// Users Register
+
+router.get('/register', function(req, res, next) {
+  res.render('register', {
+  	'title': 'Register' 
+  });
+});
+
+// Users Login
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {
+  	'title': 'Login' 
+  });
+});
+
 module.exports = router;
