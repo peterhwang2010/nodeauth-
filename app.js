b36d10c1c16ba5dd2217690c5f1faca8a5f611f7
peterhwang2010 <inhohwang2010@gmail.com>
2015-10-07 16:47:38 -0400

    finished adding validation to registration and changed the version for multer library

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // handle file uploads 
-// app.use(multer({dest: './uploads'}));
-app.use(multer({dest:'./uploads/'}).single('singleInputFileName'));
+app.use(multer({dest: './uploads'}));
+// app.use(multer({dest:'./uploads/'}).single('singleInputFileName'));
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -22,28 +24,75 @@ router.get('/login', function(req, res, next) {
   });
 });
 
-router.post('/users/register', function(req, res, next) {
+router.post('/register', function(req, res, next) {
 	var name = req.body.name;
 	var email = req.body.email;
 	var username = req.body.username;
 	var password = req.body.password;
 	var password2 = req.body.password2;
-});
+
 
 // Check for image
 
-if(req.files.profileimage){
-	console.log('Uploading File...');
+	if(req.files.profileimage){
+		console.log('Uploading File...');
 
-	// File Info 
-	var profileImageOriginalName = req.files.profileimage.originalname;
-	var profileImageName = req.files.profileimage.name;
-	var profileImageMime = req.files.profileimage.mimetype;
-	var profileImagePath = req.files.profileImage.path;
-	var profileImageExt = req.files.profileimage.extension;
-	var profileImageSize = req.files.profileimage.size;
-} else {
-	var profileImageName = 'noimage.png'
-}
+		// File Info 
+		var profileImageOriginalName = req.files.profileimage.originalname;
+		var profileImageName = req.files.profileimage.name;
+		var profileImageMime = req.files.profileimage.mimetype;
+		var profileImagePath = req.files.profileImage.path;
+		var profileImageExt = req.files.profileimage.extension;
+		var profileImageSize = req.files.profileimage.size;
+	} else {
+		var profileImageName = 'noimage.png'
+	}
+
+	req.checkBody('name', 'Name field is required').notEmpty();
+	req.checkBody('email', 'Email field is required').notEmpty();
+	req.checkBody('email', 'Email is not valid').isEmail();
+	req.checkBody('username', 'Username field is required').notEmpty();
+	req.checkBody('password', 'Password field is required').notEmpty();
+	req.checkBody('password2', 'Password do not match').equals(req.body.password);
+
+	// check for errors
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('register', {
+			errors: errors,
+			name: name,
+			email: email,
+			username: username,
+			password: password,
+			password2: password2
+		});
+	} else {
+		var newUser = new User({
+			errors: errors,
+			name: name,
+			email: email,
+			username: username,
+			password: password,
+			password2: password2,
+			profileimage: profileImageName
+		}); 
+
+	// create user
+
+	User.createUser(newUser, function(err, user){
+		if(err) throw err;
+		console.log(user);
+	});
+
+	// Success Message
+
+	req.flash('success', 'You are registered and may login');
+
+	res.location('/');
+	res.redirect('/');
+	}
+});
 
 module.exports = router;
